76695b9e6ecd0ebc432314335b4fccdccca698ba
Yagnadipsinh Chudasama <chudasamayagnadipsinh11@gmail.com>
2026-01-09 13:55:23 +0530

    Add backend Spring Boot project with Maven configuration and initial setup

---
+++ backend/backend/src/main/java/com/legal_advisor_e_fir/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.legal_advisor_e_fir.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/com/legal_advisor_e_fir/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legal_advisor_e_fir.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
